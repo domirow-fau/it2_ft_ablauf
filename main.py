809e950e578cbe4f605b83385f109a2e64a1c52c
domirow-fau <92782108+domirow-fau@users.noreply.github.com>
2022-07-27 12:43:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ with col1:
 with col2:
     col2.write("Störungsanzeige:")
     image_pos = st.empty()
-    image_pos.image("ampel_weiss.png", width=300)
+    image_pos.image("ampel_weiss.png", width=250)
 
 # introduce global variables and read json from url
 i = 0
@@ -115,75 +115,75 @@ def showDifference(prev_item, curr_item):
 
             # section for changing sensors
             elif value == "B-Referenzschalter Drehkranz (Pos. Sauger)":
-                image_anlage2.image("B-Referenzschalter Drehkranz (Pos. Sauger).png", width=300)
+                image_anlage2.image("B-Referenzschalter Drehkranz (Pos. Sauger).png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Referenzschalter Drehkranz (Pos. Foerderband)":
-                image_anlage2.image("B-Referenzschalter Drehkranz (Pos. Foerderband).png", width=300)
+                image_anlage2.image("B-Referenzschalter Drehkranz (Pos. Foerderband).png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Lichtschranke Ende Foerderband":
-                image_anlage2.image("B-Lichtschranke Ende Foerderband.png", width=300)
+                image_anlage2.image("B-Lichtschranke Ende Foerderband.png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Referenzschalter Sauger (Pos. Brennofen)":
-                image_anlage2.image("-Referenzschalter Sauger (Pos. Brennofen).png", width=300)
+                image_anlage2.image("-Referenzschalter Sauger (Pos. Brennofen).png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Lichtschranke Brennofen":
-                image_anlage2.image("B-Lichtschranke Brennofen.png", width=300)
+                image_anlage2.image("B-Lichtschranke Brennofen.png", width=250)
                 printValues(curr_item, value)
             elif value == "S-Lichtschranke Eingang":
-                image_anlage2.image("S-Lichtschranke Eingang.png", width=300)
+                image_anlage2.image("S-Lichtschranke Eingang.png", width=250)
                 printValues(curr_item, value)
             elif value == "S-Lichtschranke nach Farbsensor":
-                image_anlage2.image("S-Lichtschranke nach Farbsensor.png", width=300)
+                image_anlage2.image("S-Lichtschranke nach Farbsensor.png", width=250)
                 printValues(curr_item, value)
             elif value == "S-Lichtschranke weiss":
-                image_anlage2.image("S-Lichtschranke weiss.png", width=300)
+                image_anlage2.image("S-Lichtschranke weiss.png", width=250)
                 printValues(curr_item, value)
             elif value == "S-Lichtschranke rot":
-                image_anlage2.image("S-Lichtschranke rot.png", width=300)
+                image_anlage2.image("S-Lichtschranke rot.png", width=250)
                 printValues(curr_item, value)
             elif value == "S-Lichtschranke blau":
-                image_anlage2.image("S-Lichtschranke blau.png", width=300)
+                image_anlage2.image("S-Lichtschranke blau.png", width=250)
                 printValues(curr_item, value)
             elif value == "Lichtschranke innen":
-                image_anlage2.image("Lichtschranke innen.png", width=300)
+                image_anlage2.image("Lichtschranke innen.png", width=250)
                 printValues(curr_item, value)
             elif value == "Referenztaster vertikal":
-                image_anlage2.image("Referenztaster vertikal.png", width=300)
+                image_anlage2.image("Referenztaster vertikal.png", width=250)
                 printValues(curr_item, value)
             elif value == "Referenztaster Ausleger vorne":
-                image_anlage2.image("Referenztaster Ausleger vorne.png", width=300)
+                image_anlage2.image("Referenztaster Ausleger vorne.png", width=250)
                 printValues(curr_item, value)
             elif value == "V-Referenzschalter vertikal":
-                image_anlage4.image("V-Referenzschalter vertikal.png", width=300)
+                image_anlage4.image("V-Referenzschalter vertikal.png", width=250)
                 printValues(curr_item, value)
             elif value == "V-Referenzschalter horizontal":
-                image_anlage4.image("V-Referenzschalter horizontal.png", width=300)
+                image_anlage4.image("V-Referenzschalter horizontal.png", width=250)
                 printValues(curr_item, value)
             elif value == "V-Referenzschalter drehen":
-                image_anlage4.image("V-Referenzschalter drehen.png", width=300)
+                image_anlage4.image("V-Referenzschalter drehen.png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Motor Foerderband vorwaerts":
-                image_anlage2.image("B-Motor Foerderband vorwaerts.png", width=300)
+                image_anlage2.image("B-Motor Foerderband vorwaerts.png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Motor Saege":
-                image_anlage2.image("B-Motor Saege.png", width=300)
+                image_anlage2.image("B-Motor Saege.png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Motor Sauger zum Ofen":
-                image_anlage2.image("B-Motor Sauger zum Ofen.png", width=300)
+                image_anlage2.image("B-Motor Sauger zum Ofen.png", width=250)
                 printValues(curr_item, value)
             elif value == "B-Motor Sauger zum Drehkranz":
-                image_anlage2.image("B-Motor Sauger zum Drehkranz.png", width=300)
+                image_anlage2.image("B-Motor Sauger zum Drehkranz.png", width=250)
                 printValues(curr_item, value)
 
             # section for changing machine status
             if value == "Ampel gruen" and curr_item["werte"][value].strip() == "true":
-                image_pos.image("ampel_gruen.png", width=300)
+                image_pos.image("ampel_gruen.png", width=250)
             elif value == "Ampel weiss" and curr_item["werte"][value].strip() == "true":
-                image_pos.image("ampel_weiss.png", width=300)
+                image_pos.image("ampel_weiss.png", width=250)
             elif value == "Ampel rot" and curr_item["werte"][value].strip() == "true":
-                image_pos.image("ampel_rot.png", width=300)
+                image_pos.image("ampel_rot.png", width=250)
             elif value == "Ampel orange" and curr_item["werte"][value].strip() == "true":
-                image_pos.image("ampel_orange.png", width=300)
+                image_pos.image("ampel_orange.png", width=250)
 
 
 # MAIN ####################################################################################################################################
@@ -202,14 +202,14 @@ if button:
             image_anlage1.image("arrow_up.png", width=100)
         with col2:
             image_anlage2 = st.empty()
-            image_anlage2.image("automatisiertes_hochregallager_skaliert_1.png", width=300)
+            image_anlage2.image("automatisiertes_hochregallager_skaliert_1.png", width=250)
         with col3:
             col3.write("Bewegung Sauggreifer:")
             image_anlage3 = st.empty()
             image_anlage3.image("arrow_up.png", width=100)
         with col4:
             image_anlage4 = st.empty()
-            image_anlage4.image("vakuum_skaliert_1.png", width=300)
+            image_anlage4.image("vakuum_skaliert_1.png", width=250)
         
         col5, col6, col7 = st.columns(3)
         with col5:
